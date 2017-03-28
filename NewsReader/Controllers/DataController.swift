5c0991578c792a01220fcb88e4f61c54963795c8
Vladimír Zdražil <mail@vzdrazil.com>
2017-03-28 19:01:51 +0200

    Added DataController for Core Data

---
+++ NewsReader/Controllers/DataController.swift
@@ -0,0 +1,58 @@
+//
+//  DataController.swift
+//  NewsReader
+//
+//  Created by Vladimír Zdražil on 28/03/2017.
+//  Copyright © 2017 Vladimír Zdražil. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class DataController: NSObject {
+    
+    var managedObjectContext: NSManagedObjectContext
+    
+    // storeFileLocationURL - The directory the application uses to store the Core Data store file.
+    // modelFileName - name of the saved file
+    init(modelURL: URL, storeFileLocationURL: URL, modelFileName: String, concurrencyType: NSManagedObjectContextConcurrencyType) throws {
+        
+        // The managed object model for the application. It is a fatal error for the application not to be able to find and load its model.
+        guard let mom = NSManagedObjectModel(contentsOf: modelURL) else {
+            fatalError("Error initializing mom from: \(modelURL)")
+        }
+        let psc = NSPersistentStoreCoordinator(managedObjectModel: mom)
+        managedObjectContext = NSManagedObjectContext(concurrencyType: concurrencyType)
+        managedObjectContext.persistentStoreCoordinator = psc
+        
+        super.init()
+        
+        try createCoreDataFile(psc: psc, docURL: storeFileLocationURL, fileName: modelFileName)
+    }
+    
+    private func createCoreDataFile(psc: NSPersistentStoreCoordinator,
+                                    docURL: URL,
+                                    fileName: String) throws {
+        // Move to a background thread to do some long running work
+        DispatchQueue.global(qos: .background).async {
+            let storeURL = docURL.appendingPathComponent(fileName)
+            do {
+                try psc.addPersistentStore(ofType: NSSQLiteStoreType, configurationName: nil, at: storeURL, options: nil)
+            } catch {
+                fatalError("Error migrating store: \(error)")
+            }
+        }
+    }
+    
+    func saveContext () throws {
+        if managedObjectContext.hasChanges {
+            do {
+                try managedObjectContext.save()
+            } catch {
+                let nserror = error as NSError
+                NSLog("Unresolved error \(nserror), \(nserror.userInfo)")
+                abort()
+            }
+        }
+    }
+}
