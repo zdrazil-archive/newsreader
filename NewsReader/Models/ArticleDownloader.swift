aced93b4e29761890d5dcfd0543ede5e25ab83df
Vladimír Zdražil <mail@vzdrazil.com>
2017-03-29 13:44:07 +0200

    Add incomplete implementation of Article AlamofireObjectMapper

---
+++ NewsReader/Models/ArticleDownloader.swift
@@ -0,0 +1,16 @@
+//
+//  ArticleDownloader.swift
+//  NewsReader
+//
+//  Created by Vladimír Zdražil on 29/03/2017.
+//  Copyright © 2017 Vladimír Zdražil. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+import AlamofireObjectMapper
+
+
+class ArticleDownloader: NSObject {
+
+}
---
+++ NewsReader/Models/ArticlesResponse.swift
@@ -0,0 +1,27 @@
+//
+//  ArticlesResponse.swift
+//  NewsReader
+//
+//  Created by Vladimír Zdražil on 29/03/2017.
+//  Copyright © 2017 Vladimír Zdražil. All rights reserved.
+//
+
+import ObjectMapper
+
+struct ArticlesResponse: Mappable {
+    var status: String?
+    var source: String?
+    var sortBy: String?
+    var articles: [Article]?
+    
+    init?(map: Map) {
+        
+    }
+    
+    mutating func mapping(map: Map) {
+        status   <- map["status"]
+        source   <- map["source"]
+        sortBy   <- map["sortBy"]
+        articles <- map["articles"]
+    }
+}
