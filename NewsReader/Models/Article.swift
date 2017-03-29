98ece360f145f656cb2ed5b8aea0c3f6293a1c47
Vladimír Zdražil <mail@vzdrazil.com>
2017-03-29 08:03:42 +0200

    Rename CoreData entity, add empty Article struct

---
+++ NewsReader/Models/Article.swift
@@ -0,0 +1,13 @@
+//
+//  Article.swift
+//  NewsReader
+//
+//  Created by Vladimír Zdražil on 29/03/2017.
+//  Copyright © 2017 Vladimír Zdražil. All rights reserved.
+//
+
+import ObjectMapper
+
+struct Article {
+    
+}
