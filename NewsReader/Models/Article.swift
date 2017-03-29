b9ceda55f64aaab0e798c3fdb01402bcfd2b360a
Vladimír Zdražil <mail@vzdrazil.com>
2017-03-29 08:11:38 +0200

    Add Article properties and mappings

--- NewsReader/Models/Article.swift
+++ NewsReader/Models/Article.swift
@@ -8,6 +8,24 @@
 
 import ObjectMapper
 
-struct Article {
+struct Article: Mappable {
+    var description: String?
+    var author: String?
+    var imageURL: URL?
+    var publishedAt: Date?
+    var title: String?
+    var url: URL?
     
+    init?(map: Map) {
+        
+    }
+    
+    mutating func mapping(map: Map) {
+        description <- map["description"]
+        author      <- map["author"]
+        imageURL    <- map["urlToImage"]
+        publishedAt <- map["publishedAt"]
+        title       <- map["title"]
+        url         <- map["url"]
+    }
 }
