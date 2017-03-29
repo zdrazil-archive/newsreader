e9b28e21a7ebfc3c4f588756f9c0889a832f19d4
Vladimír Zdražil <mail@vzdrazil.com>
2017-03-29 11:39:59 +0200

    Add custom transform type for JSON date
    Parsed JSON has a different date format than the transform type that the
    ObjecMapper provides. Create custom tranform type to parse the JSON date
    string to Date correctly.

---
+++ NewsReader/Helpers/DateExtendedTransform.swift
@@ -0,0 +1,30 @@
+//
+//  DateExtendedTransform.swift
+//  NewsReader
+//
+//  Created by Vladimír Zdražil on 29/03/2017.
+//  Copyright © 2017 Vladimír Zdražil. All rights reserved.
+//
+
+import Foundation
+import ObjectMapper
+
+class DateExtendedTransform: TransformType {
+    typealias Object = Date
+    typealias JSON = Double
+    
+    func transformFromJSON(_ value: Any?) -> Date? {
+        if let timeStr = value as? String {
+            let date = Date()
+            return date.stringToISO8601Date(string: timeStr)
+        }
+        return nil
+    }
+    
+    func transformToJSON(_ value: Date?) -> Double? {
+        if let date = value {
+            return Double(date.timeIntervalSince1970)
+        }
+        return nil
+    }
+}
