a524b84ffcfa34f3a516e4dc0e4be9dc3576332d
Vladimír Zdražil <mail@vzdrazil.com>
2017-03-30 13:13:45 +0200

    Add ArticleMO extension to save JSON Article

---
+++ NewsReader/Models/ArticleCoreData/ArticleMO+Extension.swift
@@ -0,0 +1,25 @@
+//
+//  ArticleMO+Extension.swift
+//  NewsReader
+//
+//  Created by Vladimír Zdražil on 30/03/2017.
+//  Copyright © 2017 Vladimír Zdražil. All rights reserved.
+//
+
+import CoreData
+
+extension ArticleMO {
+    
+    class func createArticleMOEntity(article: Article, inManagedObjectContext context: NSManagedObjectContext) -> ArticleMO? {
+        if let articleMO = NSEntityDescription.insertNewObject(forEntityName: "ArticleMO", into: context) as? ArticleMO {
+            articleMO.author = article.author
+            articleMO.title = article.title
+            articleMO.articleDescription = article.description
+            articleMO.url = article.url?.absoluteString
+            articleMO.imageURL = article.imageURL?.absoluteString
+            articleMO.publishedAt = article.publishedAt as NSDate?
+            return articleMO
+        }
+        return nil
+    }
+}
