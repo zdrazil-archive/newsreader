b0fcf6098a49d0fcdfdf182e7198a490600aca1e
Vladimír Zdražil <mail@vzdrazil.com>
2017-03-30 14:13:05 +0200

    Change implementation of Core Data
    Follows Apple's guide, but doesn't allow easy later reuse.

--- NewsReader/Controllers/DataController.swift
+++ NewsReader/Controllers/DataController.swift
@@ -13,38 +13,27 @@ class DataController: NSObject {
     
     var managedObjectContext: NSManagedObjectContext
     
-    // storeFileLocationURL - The directory the application uses to store the Core Data store file.
-    // modelFileName - name of the saved file
-    init(modelURL: URL, storeFileLocationURL: URL, modelFileName: String, concurrencyType: NSManagedObjectContextConcurrencyType) dthrows {
-        guard let modelURL = Bundle.main.url(forResource: "ArticleModel", withExtension:"momd") else {
+    override init() {
+        // This resource is the same name as your xcdatamodeld contained in your project.
+        guard let modelURL = Bundle.main.url(forResource: "ArticleModel", withExtension: "momd") else {
             fatalError("Error loading model from bundle")
         }
-        
-        guard let storeFileLocationURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).last else {
-            fatalError("Unable to resolve document directory")
-        }
-        let modelFileName = "ArticleModel.sqlite"
-        let concurrencyType = NSManagedObjectContextConcurrencyType.mainQueueConcurrencyType
-        
         // The managed object model for the application. It is a fatal error for the application not to be able to find and load its model.
         guard let mom = NSManagedObjectModel(contentsOf: modelURL) else {
             fatalError("Error initializing mom from: \(modelURL)")
         }
         let psc = NSPersistentStoreCoordinator(managedObjectModel: mom)
-        managedObjectContext = NSManagedObjectContext(concurrencyType: concurrencyType)
+        managedObjectContext = NSManagedObjectContext(concurrencyType: .mainQueueConcurrencyType)
         managedObjectContext.persistentStoreCoordinator = psc
         
-        super.init()
-        
-        try createCoreDataFile(psc: psc, docURL: storeFileLocationURL, fileName: modelFileName)
-    }
-    
-    private func createCoreDataFile(psc: NSPersistentStoreCoordinator,
-                                    docURL: URL,
-                                    fileName: String) throws {
         // Move to a background thread to do some long running work
         DispatchQueue.global(qos: .background).async {
-            let storeURL = docURL.appendingPathComponent(fileName)
+            let urls = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+            let docURL = urls[urls.endIndex-1]
+            /* The directory the application uses to store the Core Data store file.
+             This code uses a file named "Model.sqlite" in the application's documents directory.
+             */
+            let storeURL = docURL.appendingPathComponent("ArticleModel.sqlite")
             do {
                 try psc.addPersistentStore(ofType: NSSQLiteStoreType, configurationName: nil, at: storeURL, options: nil)
             } catch {
