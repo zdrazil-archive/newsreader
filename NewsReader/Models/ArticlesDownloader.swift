671ab02e90696e0cef3c90922c1914f42bcd5691
Vladimír Zdražil <mail@vzdrazil.com>
2017-03-30 13:16:07 +0200

    Add ArticlesDownloader
    Downloads articles and saves them to database with Core Data.

---
+++ NewsReader/Models/ArticlesDownloader.swift
@@ -0,0 +1,56 @@
+//
+//  ArticlesDownloader.swift
+//  NewsReader
+//
+//  Created by Vladimír Zdražil on 29/03/2017.
+//  Copyright © 2017 Vladimír Zdražil. All rights reserved.
+//
+
+import Foundation
+import CoreData
+import Alamofire
+import AlamofireObjectMapper
+
+class ArticlesDownloader: NSObject {
+    
+    func downloadArticles(jsonURL: String)  {
+        Alamofire.request(jsonURL).responseObject { (response: DataResponse<ArticlesResponse>) in
+            
+            let articlesResponse = response.result.value
+            
+            guard articlesResponse?.status == "ok" else {
+                return
+            }
+            self.saveArticlesToCoreData(articles: articlesResponse?.articles)
+        }
+    }
+    
+    private func saveArticlesToCoreData(articles: [Article]?) {
+        let dataController = (UIApplication.shared.delegate as? AppDelegate)?.articleDataController
+        let moc = dataController?.managedObjectContext
+        let privateMOC = NSManagedObjectContext(concurrencyType: .privateQueueConcurrencyType)
+        privateMOC.parent = moc
+        
+        guard let articles = articles else {
+            return
+        }
+        
+        privateMOC.perform {
+            for article in articles {
+                _ = ArticleMO.createArticleMOEntity(article: article, inManagedObjectContext: privateMOC)
+            }
+        }
+        do {
+            try privateMOC.save()
+            moc?.performAndWait {
+                do {
+                    try moc?.save()
+                } catch {
+                    fatalError("Failure to save context: \(error)")
+                }
+            }
+        } catch let error {
+            print("Failure to save context: \(error)")
+        }
+    }
+}
