799838bb86249fd8b08876ecbb93aabbdcfff261
Vladimír Zdražil <mail@vzdrazil.com>
2017-04-02 13:15:55 +0200

    Add Core Data stack creation in closure

--- NewsReader/Controllers/DataController.swift
+++ NewsReader/Controllers/DataController.swift
@@ -12,35 +12,66 @@ import CoreData
 class DataController: NSObject {
     
     var managedObjectContext: NSManagedObjectContext
-    
-    override init() {
-        // This resource is the same name as your xcdatamodeld contained in your project.
-        guard let modelURL = Bundle.main.url(forResource: "ArticleModel", withExtension: "momd") else {
+
+    init(completionClosure: @escaping () -> ()) {
+        //This resource is the same name as your xcdatamodeld contained in your project
+        guard let modelURL = Bundle.main.url(forResource: "ArticleModel", withExtension:"momd") else {
             fatalError("Error loading model from bundle")
         }
         // The managed object model for the application. It is a fatal error for the application not to be able to find and load its model.
         guard let mom = NSManagedObjectModel(contentsOf: modelURL) else {
             fatalError("Error initializing mom from: \(modelURL)")
         }
+
         let psc = NSPersistentStoreCoordinator(managedObjectModel: mom)
-        managedObjectContext = NSManagedObjectContext(concurrencyType: .mainQueueConcurrencyType)
+
+        managedObjectContext = NSManagedObjectContext(concurrencyType: NSManagedObjectContextConcurrencyType.mainQueueConcurrencyType)
         managedObjectContext.persistentStoreCoordinator = psc
-        
-        // Move to a background thread to do some long running work
-        DispatchQueue.global(qos: .background).async {
-            let urls = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
-            let docURL = urls[urls.endIndex-1]
-            /* The directory the application uses to store the Core Data store file.
-             This code uses a file named "Model.sqlite" in the application's documents directory.
-             */
+
+        let queue = DispatchQueue.global(qos: DispatchQoS.QoSClass.background)
+        queue.async {
+            guard let docURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).last else {
+                fatalError("Unable to resolve document directory")
+            }
             let storeURL = docURL.appendingPathComponent("ArticleModel.sqlite")
             do {
                 try psc.addPersistentStore(ofType: NSSQLiteStoreType, configurationName: nil, at: storeURL, options: nil)
+                //The callback block is expected to complete the User Interface and therefore should be presented back on the main queue so that the user interface does not need to be concerned with which queue this call is coming from.
+                DispatchQueue.main.sync(execute: completionClosure)
             } catch {
                 fatalError("Error migrating store: \(error)")
             }
         }
     }
+//    
+//    override init() {
+//        // This resource is the same name as your xcdatamodeld contained in your project.
+//        guard let modelURL = Bundle.main.url(forResource: "ArticleModel", withExtension: "momd") else {
+//            fatalError("Error loading model from bundle")
+//        }
+//        // The managed object model for the application. It is a fatal error for the application not to be able to find and load its model.
+//        guard let mom = NSManagedObjectModel(contentsOf: modelURL) else {
+//            fatalError("Error initializing mom from: \(modelURL)")
+//        }
+//        let psc = NSPersistentStoreCoordinator(managedObjectModel: mom)
+//        managedObjectContext = NSManagedObjectContext(concurrencyType: .mainQueueConcurrencyType)
+//        managedObjectContext.persistentStoreCoordinator = psc
+//        
+//        // Move to a background thread to do some long running work
+//        DispatchQueue.global(qos: .background).async {
+//            let urls = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+//            let docURL = urls[urls.endIndex-1]
+//            /* The directory the application uses to store the Core Data store file.
+//             This code uses a file named "Model.sqlite" in the application's documents directory.
+//             */
+//            let storeURL = docURL.appendingPathComponent("ArticleModel.sqlite")
+//            do {
+//                try psc.addPersistentStore(ofType: NSSQLiteStoreType, configurationName: nil, at: storeURL, options: nil)
+//            } catch {
+//                fatalError("Error migrating store: \(error)")
+//            }
+//        }
+//    }
     
     func saveContext() {
         if managedObjectContext.hasChanges {
