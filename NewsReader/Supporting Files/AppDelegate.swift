9d745367fc700c2d9561d2485b58153781b2c1d6
Vladimír Zdražil <mail@vzdrazil.com>
2017-04-02 09:38:43 +0200

    Add statusBarStyle with light content

--- NewsReader/Supporting Files/AppDelegate.swift
+++ NewsReader/Supporting Files/AppDelegate.swift
@@ -17,6 +17,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Initialize Core Data Stack
         dataController = DataController()
+        UIApplication.shared.statusBarStyle = .lightContent
+//        UIApplication.shared.Navi
         
         return true
     }
