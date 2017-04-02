9133f59482d914271e463c0c9c729b1ae394132f
Vladimír Zdražil <mail@vzdrazil.com>
2017-04-02 13:16:26 +0200

    Add UI setup in dataController closure

--- NewsReader/Supporting Files/AppDelegate.swift
+++ NewsReader/Supporting Files/AppDelegate.swift
@@ -16,8 +16,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Initialize Core Data Stack
-        dataController = DataController()
+        dataController = DataController {
         UIApplication.shared.statusBarStyle = .lightContent
+        }
 //        UIApplication.shared.Navi
         
         return true
