522db58ce8cb20122c35bd36c2091fabbfad9da8
Vladimír Zdražil <mail@vzdrazil.com>
2017-04-10 09:40:00 +0200

    Add option to create in-memory MOC
    In DataController. For easier testing.

--- NewsReader/Controllers/DataController.swift
+++ NewsReader/Controllers/DataController.swift
@@ -53,4 +53,21 @@ class DataController: NSObject {
             }
         }
     }
+
+    func setUpInMemoryManagedObjectContext() -> NSManagedObjectContext {
+        let managedObjectModel = NSManagedObjectModel.mergedModel(from: [Bundle.main])!
+
+        let persistentStoreCoordinator = NSPersistentStoreCoordinator(managedObjectModel: managedObjectModel)
+
+        do {
+            try persistentStoreCoordinator.addPersistentStore(ofType: NSInMemoryStoreType, configurationName: nil, at: nil, options: nil)
+        } catch {
+            print("Adding in-memory persistent store failed")
+        }
+
+        let managedObjectContext = NSManagedObjectContext(concurrencyType: .mainQueueConcurrencyType)
+        managedObjectContext.persistentStoreCoordinator = persistentStoreCoordinator
+        
+        return managedObjectContext
+    }
 }
